6ef818b8cb19e0af6cccd6df03f3db6fd8710e7a
Gealor <gzazulya@gmail.com>
2026-04-26 12:06:57 +0400

    refactor/add-annotation-on-return-value-in-get-db-dependency

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,12 +1,13 @@
 from fastapi import Depends
 
+from core.database import Database
 from core.database import db
 from repositories.tasks.base_repository import BaseTaskRepository
 from repositories.tasks.in_memory_repository import TaskInMemoryRepository
 from services.tasks_service import TaskService
 
 
-def get_db():
+def get_db() -> Database:
     return db
 
 def get_task_repository() -> BaseTaskRepository:
